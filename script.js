e5a62f5222b248d9fd0a7a879f448f04f173cf52
Mahek shah <149513184+mahekshah429@users.noreply.github.com>
2025-06-23 08:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const message = document.createElement("p");
-  message.textContent = "DOM load success";
-  document.body.appendChild(message);
+  const p = document.createElement("p");
+  p.textContent = "DOM load success";
+  document.body.appendChild(p);
 });
